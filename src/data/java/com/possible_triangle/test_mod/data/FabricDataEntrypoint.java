f0d93cbc2feb78d887f530a8cb86080d2a264d04
Niklas Widmann <nik@somethingcatchy.net>
2026-05-26 14:57:47 +0200

    update to 26.1.2

--- src/data/java/com/possible_triangle/test_mod/data/FabricDataEntrypoint.java
+++ src/data/java/com/possible_triangle/test_mod/data/FabricDataEntrypoint.java
@@ -1,6 +1,5 @@
 package com.possible_triangle.test_mod.data;
 
-import java.util.Optional;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.DetectedVersion;
@@ -8,6 +7,7 @@ import net.minecraft.data.metadata.PackMetadataGenerator;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.metadata.pack.PackMetadataSection;
+import net.minecraft.util.InclusiveRange;
 
 public class FabricDataEntrypoint implements DataGeneratorEntrypoint {
 
@@ -15,10 +15,9 @@ public class FabricDataEntrypoint implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         var pack = generator.createPack();
 
-        pack.addProvider((output, $) -> new PackMetadataGenerator(output).add(PackMetadataSection.TYPE, new PackMetadataSection(
-                Component.literal("test mod resources"),
-                DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
-                Optional.empty()
+        pack.addProvider((output, $) -> new PackMetadataGenerator(output).add(PackMetadataSection.SERVER_TYPE, new PackMetadataSection(
+            Component.literal("test mod resources"),
+            new InclusiveRange<>(DetectedVersion.BUILT_IN.packVersion(PackType.CLIENT_RESOURCES))
         )));
     }
 
