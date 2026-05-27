04b3d4a4dc6315da38417e00552c18c14374bf20
Niklas Widmann <nik@somethingcatchy.net>
2026-05-27 10:52:26 +0200

    update to 26.1.2

--- common/src/main/java/com/possible_triangle/test_mod/Constants.java
+++ common/src/main/java/com/possible_triangle/test_mod/Constants.java
@@ -1,6 +1,6 @@
 package com.possible_triangle.test_mod;
 
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,8 +8,8 @@ public class Constants {
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "test_mod";
 
-    public static ResourceLocation createId(String location) {
-        return ResourceLocation.fromNamespaceAndPath(MOD_ID, location);
+    public static Identifier createId(String location) {
+        return Identifier.fromNamespaceAndPath(MOD_ID, location);
     }
 
 
